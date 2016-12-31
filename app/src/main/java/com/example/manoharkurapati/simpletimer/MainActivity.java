47abdfc42dc455d97ec99d6ecd0eb9e21ea87d59
ManoharKurapati <kvmanohar@gmail.com>
2016-12-31 20:13:35 +0000

    -Use Text Switcher to perform text animation

--- app/src/main/java/com/example/manoharkurapati/simpletimer/MainActivity.java
+++ app/src/main/java/com/example/manoharkurapati/simpletimer/MainActivity.java
@@ -1,15 +1,19 @@
 package com.example.manoharkurapati.simpletimer;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
+import android.widget.TextSwitcher;
 import android.widget.TextView;
+import android.widget.ViewSwitcher;
 
 import org.w3c.dom.Text;
 
@@ -18,6 +22,8 @@ public class MainActivity extends Activity {
     private CountDownTimer countDownTimer;
 
     private TextView textView;
+    private TextSwitcher textSwitcher;
+
     private Button btnStart;
     private Button btnReset;
     private ProgressBar mProgressBar;
@@ -29,24 +35,39 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = (TextView) findViewById(R.id.textView);
+        textSwitcher = (TextSwitcher) findViewById(R.id.textSwitcher);
+
         btnReset = (Button) findViewById(R.id.btnReset);
         btnStart = (Button) findViewById(R.id.btnStart);
         mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
 
+        //Get Alarm sound file Uri and set it to Ringtone ready to play
         notificationUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
         ringtone = RingtoneManager.getRingtone(this,notificationUri);
+
+        //Setup TextSwitcher
+        textSwitcher.setFactory(new ViewSwitcher.ViewFactory(){
+            @Override
+            public View makeView() {
+                textView = new TextView(MainActivity.this);
+                textView.setTextColor(Color.BLUE);
+                textView.setTextSize(80);
+                textView.setGravity(Gravity.CENTER_HORIZONTAL);
+                return textView;
+            }
+        });
+
 
         countDownTimer = new CountDownTimer(5000, 1000) {
             @Override
             public void onTick(long l) {
-                textView.setText("" + l / 1000);
+                textSwitcher.setText("" + l/1000);
                 mProgressBar.incrementProgressBy(1);
             }
 
             @Override
             public void onFinish() {
-                textView.setText("TIME-UP!!");
+                textSwitcher.setText("TIME-UP!!");
                 mProgressBar.setProgress(5);
                 ringtone.play();
             }
@@ -64,11 +85,13 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View view) {
                 countDownTimer.cancel();
-                textView.setText("5");
+                textSwitcher.setText("5");
                 mProgressBar.setProgress(0);
                 ringtone.stop();
             }
         });
+
+
 
     }
 }
